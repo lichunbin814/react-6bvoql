d8a44515e35242cb5ea17c548d44abdc313e2f97
lichunbin814 <lichunbin814@gmail.com>
2023-01-09 23:07:21 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import * as topojson from 'topojson';
 
 const Map = () => {
   // Set dimensions of the map
-  const width = 960;
-  const height = 500;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
   // Use useRef hook to get a reference to the map container
   const mapContainer = React.useRef(null);
@@ -17,17 +17,18 @@ const Map = () => {
       (topoData) => {
         // Create a projection
         const projection = d3
-          .geoMercator()
-          .center([123, 24])
-          .scale(5500)
-          .translate([width / 2, height / 2])
-          .precision(0.1);
+          .geoMercator() // 麥卡托投影法
+          .center([121, 24]) // 中心點(經緯度)
+          .scale(6000) // 放大倍率
+          .translate([width / 2, height / 2.5]); // 置中
 
         // Create a path generator
         const pathGenerator = d3.geoPath().projection(projection);
 
         // Convert TopoJSON to GeoJSON (target object = 'countries')
         const geoJson = topojson.feature(topoData, topoData.objects.counties);
+
+        let previous = null;
 
         // Bind data and create one path per GeoJSON feature
         d3.select(mapContainer.current)
@@ -36,9 +37,26 @@ const Map = () => {
           .enter()
           .append('path')
           .attr('d', pathGenerator)
-          .style('fill', '#ccc')
+          .style('fill', (d) => {
+            // Set fill color for specific counties
+            if (
+              d.properties.COUNTYNAME === '台北市' ||
+              d.properties.COUNTYNAME === '新北市'
+            ) {
+              return 'red';
+            }
+            // Set default fill color for other counties
+            return '#ccc';
+          })
           .style('stroke', '#fff')
-          .style('stroke-width', '1');
+          .style('stroke-width', '1')
+          .on('click', function (event, data) {
+            if (previous) {
+              d3.select(previous).style('fill', '#ccc');
+            }
+            d3.select(this).style('fill', '#ff1a75');
+            previous = this;
+          });
       }
     );
   }, []);
@@ -46,7 +64,7 @@ const Map = () => {
   return (
     <svg
       ref={mapContainer}
-      viewBox="0 0 960 500"
+      viewBox={`0 0 ${width} ${height}`}
       width={width}
       height={height}
       style={{ width: '100%', height: '100%' }}
